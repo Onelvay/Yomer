3be1926199671981e2a16b43b6fb5a42a590bae6
Onelvay <abajalen@gmail.com>
2023-04-15 20:04:16 +0600

    frst commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProfileComponent } from './profile/profile.component';
+import { VacancyComponent } from './vacancy/vacancy.component';
+import { WhoCreatedComponent } from './who-created/who-created.component';
+const routes: Routes = [
+  {path:'',component:MainPageComponent,children:[
+    {path:'',redirectTo:'/',pathMatch:'full'},
+    {path:'',component:WhoCreatedComponent},
+    { path:'profile',component:ProfileComponent,},
+    { path:'vacancy',component:VacancyComponent}
+  ]},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { VacancyComponent } from './vacancy/vacancy.component';
+import { ProfileComponent } from './profile/profile.component';
+import { WhoCreatedComponent } from './who-created/who-created.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    VacancyComponent,
+    ProfileComponent,
+    WhoCreatedComponent
   ],
   imports: [
     BrowserModule,
